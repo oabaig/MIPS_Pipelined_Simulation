d25f2317b69272d6b23862ede8caac0738aff39e
nicoleorg123 <62865697+nicoleorg123@users.noreply.github.com>
2020-03-30 18:43:15 -0500

    Update MIPS_Pipelined_Simulation.cpp

--- MIPS_Pipelined_Simulation.cpp
+++ MIPS_Pipelined_Simulation.cpp
@@ -53,3 +53,89 @@ void fileInput(ifstream &fpi, REGISTERS &R) {
 	//	}
 	}*/
 }
+
+/*
+#include "mips_pipeline.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// global variables
+vector<string> instruction_set;
+vector<int> registers;
+vector<int> memory;
+int inst_num;
+IFID ifid;
+IDEX idex;
+EXMEM exmem;
+MEMWB memwb;
+
+
+// read file and initialize regsiters & memory file
+bool read_file(string filename) { 
+	ifstream fin(filename);
+	if (!fin.is_open()) {
+		cout << "ERROR: file not open\n";
+		return false;
+	}
+
+	string str;
+	int num;
+	bool regstart = false, memstart = false, codestart = false;
+	while (fin >> str) {
+		if (str == "REGISTERS") {
+			regstart = true;
+		}
+		else if (str == "MEMORY") {
+			regstart = false;
+			memstart = true;
+		}
+		else if (str == "CODE") {
+			memstart = false;
+			codestart = true;
+		}
+		else if (regstart) {
+			fin >> num;
+			str.erase(0,1);
+			registers.at(stoi(str)) = num;
+		}
+		else if (memstart) {
+			fin >> num;
+			memory.at((stoi(str) / 4)) = num;
+		}
+		else if (codestart) {
+			instruction_set.push_back(str);
+		}
+	}
+	fin.close();
+
+	cout << "Registers\n";
+	for (int i = 0; i < registers.size(); i++) {
+		cout << "R" << i << " " << registers.at(i) << endl;
+	}
+	cout << "Memory\n";
+	for (int i = 0; i < memory.size(); i++) {
+		cout << i*4 << " " << memory.at(i) << endl;
+	}
+	cout << "Instruction Set\n";
+	for (int i = 0; i < instruction_set.size(); i++) {
+		cout << instruction_set.at(i) << endl;
+	}
+
+	return true;
+}
+
+int main() {
+	Init_Registers();	// initialize register file to 0
+	Init_Memory(5);	// initialize memory file 5*4 bytes
+
+	string infilename = "input.txt";
+
+	// read file
+	read_file(infilename);
+
+	return 0;
+}
+*/
