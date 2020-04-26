d3e30d506d600f1dcec0950fb612332eab754bdd
nicoleorg123 <62865697+nicoleorg123@users.noreply.github.com>
2020-04-26 16:13:06 -0500

    Update MIPS_Pipelined_Simulation.cpp

--- MIPS_Pipelined_Simulation.cpp
+++ MIPS_Pipelined_Simulation.cpp
@@ -55,7 +55,7 @@ void fileInput(ifstream &fpi, REGISTERS &R) {
 }
 
 /*
-#include "mips_pipeline.h"
+#include "mips.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -66,12 +66,10 @@ using namespace std;
 vector<string> instruction_set; // use program counter to step to next instruction
 vector<int> registers;
 vector<int> memory;
-int inst_num;
 IFID ifid;
 IDEX idex;
 EXMEM exmem;
 MEMWB memwb;
-
 
 // read file and initialize regsiters & memory file
 bool read_file(string filename) { 
@@ -111,6 +109,7 @@ bool read_file(string filename) {
 	}
 	fin.close();
 
+	cout << "========== INPUT ==========\n";
 	cout << "Registers\n";
 	for (int i = 0; i < registers.size(); i++) {
 		if(registers.at(i))
@@ -125,17 +124,20 @@ bool read_file(string filename) {
 	for (int i = 0; i < instruction_set.size(); i++) {
 		cout << instruction_set.at(i) << endl;
 	}
+	cout << "===============================================\n";
 
 	return true;
 }
 
 int main() {
 	string infilename, outfilename;
+	infilename = "input.txt";
+	outfilename = "output.txt";
 
-	cout << "Enter input filename: ";
-	cin >> infilename;
-	cout << "Enter output filename: ";
-	cin >> outfilename;
+//	cout << "Enter input filename: ";
+//	cin >> infilename;
+//	cout << "Enter output filename: ";
+//	cin >> outfilename;
 
 	// initialization
 	Init_Registers();	// initialize all register value to 0
@@ -143,31 +145,62 @@ int main() {
 
 	// read file
 	read_file(infilename);
-	
+
+	// output file
+	ofstream fout(outfilename);
+
 	// initialize program counter
 	ifid.pc = 0;
-	
-	while (idex.pc < (instruction_set.size() * 4)) {
-		IF(instruction_set.at(ifid.pc / 4));
-		ID();
-		EX();
-		MEM();
-		WB();
-	}
 
-	cout << "=============== FINAL ================\n";
+	int cnt = 0, inst_num = 0;
+	bool isMEM, isWB, branch;
+	while (idex.pc < (instruction_set.size() * 4)) {
+		// initialize
+		inst_num = (ifid.pc / 4);
+		isMEM = isWB = true;
+		branch = false;
+
+		// instructions
+		IF(instruction_set.at(inst_num)); cnt++;
+		cout << "C#" << cnt << " " << "I" << inst_num + 1 << "-IF" << endl;
+		fout << "C#" << cnt << " " << "I" << inst_num + 1 << "-IF" << endl;
+		ID(); cnt++;
+		cout << "C#" << cnt << " " << "I" << inst_num + 1 << "-ID" << endl;
+		fout << "C#" << cnt << " " << "I" << inst_num + 1 << "-ID" << endl;
+		isMEM = EX(); cnt++;
+		cout << "C#" << cnt << " " << "I" << inst_num + 1 << "-EX" << endl;
+		fout << "C#" << cnt << " " << "I" << inst_num + 1 << "-EX" << endl;
+		if (isMEM) {
+			isWB = MEM(); cnt++;
+			cout << "C#" << cnt << " " << "I" << inst_num + 1 << "-MEM" << endl;
+			fout << "C#" << cnt << " " << "I" << inst_num + 1 << "-MEM" << endl;
+		}
+		isWB = MEM();
+		if (isWB) {
+			WB(); cnt++;
+			cout << "C#" << cnt << " " << "I" << inst_num + 1 << "-WB" << endl;
+			fout << "C#" << cnt << " " << "I" << inst_num + 1 << "-WB" << endl;
+		}
+	}
+	cout << "=========== FINAL OUTPUT ===========\n";
 	cout << "Registers\n";
+	fout << "Registers\n";
 	for (int i = 0; i < registers.size(); i++) {
-		if (registers.at(i))
+		if (registers.at(i)) {
 			cout << "R" << i << " " << registers.at(i) << endl;
+			fout << "R" << i << " " << registers.at(i) << endl;
+		}
 	}
 	cout << "Memory\n";
+	fout << "Memory\n";
 	for (int i = 0; i < memory.size(); i++) {
-		if (memory.at(i))
+		if (memory.at(i)) {
 			cout << i * 4 << " " << memory.at(i) << endl;
+			fout << i * 4 << " " << memory.at(i) << endl;
+		}
 	}
-
-
+	
+	fout.close();
 	return 0;
 }
 */
