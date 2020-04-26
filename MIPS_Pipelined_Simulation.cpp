840475e676d2028324568524ed84de4e69c61e38
nicoleorg123 <62865697+nicoleorg123@users.noreply.github.com>
2020-04-26 18:49:32 -0500

    Update MIPS_Pipelined_Simulation.cpp

--- MIPS_Pipelined_Simulation.cpp
+++ MIPS_Pipelined_Simulation.cpp
@@ -1,60 +1,4 @@
 // MIPS_Pipelined_Simulation.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include "pch.h"
-#include "MIPS_Resources.h"
-#include <fstream>
-#include <string>
-#include <sstream>
-using namespace std;
-
-void fileInput(ifstream &fpi, REGISTERS &R);
-
-int main()
-{
-	string filename;
-	ifstream fpi;
-	REGISTERS R;
-
-	cout << "Input the file name: ";
-	cin >> filename;
-	cout << endl;
-	fpi.open(filename);
-	fileInput(fpi, R);
-}
-
-void fileInput(ifstream &fpi, REGISTERS &R) {
-	string line;
-	string reg, memLoc, contents, code;
-
-	getline(fpi, line);
-	while (!fpi.eof()) {
-		if (line == "REGISTERS") {
-			while (line != "MEMORY") {
-				getline(fpi, line);
-				stringstream ss(line);
-				ss >> reg >> contents;
-				cout << reg << " " << contents << " || " << line << endl;
-			}
-		}
-	}
-	/*
-	getline(fpi, inputText);
-	while (!fpi.eof()) {
-
-	//	if (inputText == "REGISTERS") {
-			getline(fpi, inputText);
-		//	while (inputText != "MEMORY") {
-				//getline(fpi, inputText);
-				temp << inputText;
-				temp >> temp2;
-				cout << temp2 << endl;
-		//	}
-	//	}
-	}*/
-}
-
-/*
 #include "mips.h"
 #include <iostream>
 #include <fstream>
@@ -203,4 +147,3 @@ int main() {
 	fout.close();
 	return 0;
 }
-*/
